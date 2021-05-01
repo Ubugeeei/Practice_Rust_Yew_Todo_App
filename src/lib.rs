bcaa2f69585e1e3e255462e9da429722eb3b765a
Ubugeeei <ubuge1122@gmail.com>
2021-05-01 17:35:25 +0900

    ADD: 一通り完成

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use wasm_bindgen::prelude::*;
-use yew::events::MouseEvent;
 use yew::{html, Component, ComponentLink, Html, InputData, ShouldRender};
 
 pub struct TodoApp {
@@ -9,35 +8,81 @@ pub struct TodoApp {
 pub struct State {
     todos: Vec<Todo>,
     new_todo_description: String,
-    new_todo_id: usize
+    new_todo_id: usize,
+    is_showed_edit_input: bool,
+    edit_todo_id: usize,
 }
 struct Todo {
     id: usize,
     description: String,
 }
 pub enum Msg {
+    Nope,
+    UpdateInput(String),
     AddTodo,
     DeleteTodo(usize),
     EditTodo(usize),
-    UpdateInput(String),
-    Nope,
+    ShowEditInput(usize, String),
 }
 
 /**
- * create element
+ *  methods
+ */
+impl State {
+    fn add_todo(&mut self) {
+        if !self.new_todo_description.is_empty() {
+            let description = self.new_todo_description.trim();
+            self.todos.push(Todo {
+                id: self.new_todo_id,
+                description: description.to_string(),
+            });
+            self.new_todo_id += 1;
+            self.reset_form();
+        }
+    }
+
+    fn delete_todo(&mut self, id: usize) {
+        let nwe_list = self.todos.drain(..).filter(|todo| todo.id != id).collect();
+        self.todos = nwe_list;
+    }
+
+    fn edit_todo(&mut self, id: usize) {
+        let mut target_todo_index = 0;
+        for (index, todo) in self.todos.iter().enumerate() {
+            if todo.id == id {
+                target_todo_index = index
+            }
+        }
+        let new_description = self.new_todo_description.to_string();
+        self.todos[target_todo_index].description = new_description;
+        self.reset_form();
+    }
+
+    fn reset_form(&mut self){
+        self.is_showed_edit_input = false;
+        self.edit_todo_id = 0;
+        self.new_todo_description = "".to_string();
+    }
+}
+
+/**
+ *  Model
+ *  create element
  */
 impl TodoApp {
     fn view_todo(&self, (_, todo): (usize, &Todo)) -> Html {
+        let todo_description = todo.description.to_string();
         let todo_id = todo.id;
         html! {
-            <li class="todo">
-                <p>{ &todo.description }</p>
-                <button onclick=self.link.callback(move |_| Msg::DeleteTodo(todo_id))>{ "delete" }</button>
+            <li class="todo" style="display: flex;">
+                <p style="margin-right: 20px;">{ &todo.description }</p>
+                <button onclick=&self.link.callback(move |_| Msg::DeleteTodo(todo_id)) style="margin-right: 20px;">{ "delete" }</button>
+                <button onclick=&self.link.callback(move |_| Msg::ShowEditInput(todo_id, String::from(&todo_description)))>{ "edit" }</button>
             </li>
         }
     }
 
-    fn view_form(&self) -> Html {
+    fn view_add_form(&self) -> Html {
         html! {
             <div id="add-todo-form">
                 <input
@@ -45,25 +90,43 @@ impl TodoApp {
                     placeholder="add new!"
                     value=&self.state.new_todo_description
                 />
-                <button onclick=self.link.callback(|e: MouseEvent| Msg::AddTodo)>{ "Add" }</button>
+                <button onclick=self.link.callback(|_| Msg::AddTodo)>{ "Add" }</button>
+            </div>
+        }
+    }
+
+    fn view_edit_form(&self) -> Html {
+        let edit_todo_id = self.state.edit_todo_id;
+        html! {
+            <div id="edit-todo-form">
+                <input
+                    oninput=self.link.callback(|e: InputData| Msg::UpdateInput(e.value))
+                    placeholder="add new!"
+                    value=&self.state.new_todo_description
+                />
+                <button onclick=self.link.callback(move |_| Msg::EditTodo(edit_todo_id))>{ "Save" }</button>
             </div>
         }
     }
 }
 
 /**
- * main component
+ *  main component
  */
 impl Component for TodoApp {
     type Message = Msg;
     type Properties = ();
-
+    /**
+     *  Life cycle
+     */
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         let todos = Vec::new();
         let state = State {
             todos,
             new_todo_description: "".into(),
-            new_todo_id: 0
+            new_todo_id: 1,
+            is_showed_edit_input: false,
+            edit_todo_id: 0,
         };
 
         TodoApp { state, link }
@@ -71,27 +134,25 @@ impl Component for TodoApp {
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddTodo => {
-                if !self.state.new_todo_description.is_empty() {
-                    let description = self.state.new_todo_description.trim();
-                    self.state.todos.push(Todo {
-                        id: self.state.new_todo_id,
-                        description: description.to_string(),
-                    });
-                    self.state.new_todo_id += 1;
-                    self.state.new_todo_description = "".to_string();
-                }
-            }
-            Msg::EditTodo(id) => {}
-            Msg::DeleteTodo(id) => {
-                // self.state.todos.remove(id);
-                self.state.todos.into_iter().filter(|todo| todo.id != id);
-            }
-
+            Msg::Nope => {}
             Msg::UpdateInput(val) => {
                 self.state.new_todo_description = val;
             }
-            Msg::Nope => {}
+
+            Msg::AddTodo => {
+                self.state.add_todo()
+            }
+            Msg::DeleteTodo(id) => {
+                self.state.delete_todo(id);
+            }
+            Msg::EditTodo(id) => {
+                self.state.edit_todo(id)
+            }
+            Msg::ShowEditInput(todo_id, description) => {
+                self.state.is_showed_edit_input = true;
+                self.state.edit_todo_id = todo_id;
+                self.state.new_todo_description = description;
+            }
         }
         true
     }
@@ -101,14 +162,22 @@ impl Component for TodoApp {
     }
 
     /**
-     * templates
+     *  views
      */
     fn view(&self) -> Html {
         html! {
             <div id="todo-app">
                 <h2>{ "Rust Todo App" }</h2>
-                // form
-                { self.view_form() }
+
+                // input form
+                {
+                    if !self.state.is_showed_edit_input {
+                        self.view_add_form()
+                    } else {
+                        self.view_edit_form()
+                    }
+                }
+
                 // list
                 <ul>
                     { for self.state.todos.iter().enumerate().map(|e| self.view_todo(e)) }
@@ -118,6 +187,9 @@ impl Component for TodoApp {
     }
 }
 
+/**
+ *  start application
+ */
 #[wasm_bindgen(start)]
 pub fn run_app() {
     yew::start_app::<TodoApp>();
